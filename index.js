0fd9a8957b5e232805875f87988d2cfd29e52fd6
Abdul Momin <abdulmomin114477@gmail.com>
2020-08-19 21:37:17 +0600

    all

--- index.js
+++ index.js
@@ -7,65 +7,71 @@ const laptopData = JSON.parse(
   fs.readFileSync(`${__dirname}/data/data.json`, "utf-8")
 );
 
-const server = http.createServer((req, res) => {
-  // URLs & IDs
-  const requrl = url.parse(req.url, true);
-  const pathName = requrl.pathname;
-  const id = requrl.query.id;
+const server = http
+  .createServer((req, res) => {
+    // URLs & IDs
+    const requrl = url.parse(req.url, true);
+    const pathName = requrl.pathname;
+    const id = requrl.query.id;
 
-  // Home page or products page route
-  if (pathName === "/products" || pathName === "/") {
-    res.writeHead(200, { "Content-type": "text/html" });
+    // Home page or products page route
+    if (pathName === "/products" || pathName === "/") {
+      res.writeHead(200, { "Content-type": "text/html" });
 
-    fs.readFile(
-      `${__dirname}/templates/overview.html`,
-      "utf-8",
-      (err, data) => {
-        let output = data;
+      fs.readFile(
+        `${__dirname}/templates/overview.html`,
+        "utf-8",
+        (err, data) => {
+          let output = data;
 
-        fs.readFile(
-          `${__dirname}/templates/card.html`,
-          "utf-8",
-          (err, data) => {
-            const cardsOutput = laptopData
-              .map((el) => {
-                return replaceTemplate(data, el);
-              })
-              .join("");
+          fs.readFile(
+            `${__dirname}/templates/card.html`,
+            "utf-8",
+            (err, data) => {
+              const cardsOutput = laptopData
+                .map((el) => {
+                  return replaceTemplate(data, el);
+                })
+                .join("");
 
-            output = output.replace("{%CARDS%}", cardsOutput);
+              output = output.replace("{%CARDS%}", cardsOutput);
 
-            res.end(output);
-          }
-        );
-      }
-    );
+              res.end(output);
+            }
+          );
+        }
+      );
 
-    // Single procuct page route
-  } else if (pathName === "/laptop") {
-    res.writeHead(200, { "Content-type": "text/html" });
+      // Single procuct page route
+    } else if (pathName === "/laptop") {
+      res.writeHead(200, { "Content-type": "text/html" });
 
-    fs.readFile(`${__dirname}/templates/laptop.html`, "utf-8", (err, data) => {
-      const laptop = laptopData[id];
-      const output = replaceTemplate(data, laptop);
+      fs.readFile(
+        `${__dirname}/templates/laptop.html`,
+        "utf-8",
+        (err, data) => {
+          const laptop = laptopData[id];
+          const output = replaceTemplate(data, laptop);
 
-      res.end(output);
-    });
-  } else if (/\.(jpg|jpeg|png|ico|gif)$/i.test(pathName)) {
-    fs.readFile(`${__dirname}/data/img${pathName}`, (err, data) => {
-      res.writeHead(200, { "Content-type": "image/jpg" });
+          res.end(output);
+        }
+      );
+    } else if (/\.(jpg|jpeg|png|ico|gif)$/i.test(pathName)) {
+      fs.readFile(`${__dirname}/data/img${pathName}`, (err, data) => {
+        res.writeHead(200, { "Content-type": "image/jpg" });
 
-      res.end(data);
-    });
-  }
-  // 404 Error Message
-  else {
-    res.writeHead(404, { "Content-type": "text/html" });
-    res.end("URL not found : ( ");
-  }
-});
+        res.end(data);
+      });
+    }
+    // 404 Error Message
+    else {
+      res.writeHead(404, { "Content-type": "text/html" });
+      res.end("URL not found : ( ");
+    }
+  })
+  .listen(PORT);
 
-server.listen(process.env.PORT, "localhost", () => {});
+// server.listen(PORT, "localhost", () => {});
 
 const replaceTemplate = (originalHtml, laptop) => {
   let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
